7c5cf9674fd964a2fa5835502a3d4aaf9f2175c2
thiago-drigues <thg.drgs@gmail.com>
2020-06-05 11:33:13 -0300

    wp-stormy theme

--- footer.php
+++ footer.php
@@ -2,17 +2,41 @@
             <div class="wrapper-9 go-wrapper">
                 <div class="content-9 go-content container-md container-sm">
                     <div class="footer-link">
-                        <h1>Stormy</h1>
-                        <ul>
-                            <li><a href="#">Vantagens</a></li>
-                            <li><a href="#">Características</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Fale Conosco</a></li>
-                        </ul>
-                        <ul>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Instagram</a></li>
-                        </ul>
+                        <nav class="navbar navbar-expand-lg">
+                            <div class="logo-footer">
+                                <a class="navbar-brand" href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="191.74" height="39.087" viewBox="0 0 191.74 39.087"><g transform="translate(-679.48 -286.736)"><g transform="translate(723.316 286.736)"><path d="M792.711,315.938a15.658,15.658,0,0,1-5.231-2.348l2.431-5.478a18.144,18.144,0,0,0,4.551,2.08,16.787,16.787,0,0,0,4.839.721,8.46,8.46,0,0,0,3.789-.679,2.027,2.027,0,0,0,.124-3.748,16.326,16.326,0,0,0-4.489-1.421,24.439,24.439,0,0,1-6.467-2.039,7.625,7.625,0,0,1-3.233-2.863,8.033,8.033,0,0,1-.927-4,8.2,8.2,0,0,1,1.524-4.839,10.033,10.033,0,0,1,4.284-3.378,15.5,15.5,0,0,1,6.3-1.215,21.553,21.553,0,0,1,6.158.886,12.728,12.728,0,0,1,4.675,2.327l-2.389,5.478a15.94,15.94,0,0,0-4.078-2.08,13.374,13.374,0,0,0-4.284-.721,6.272,6.272,0,0,0-3.377.8,2.451,2.451,0,0,0-1.277,2.162,1.758,1.758,0,0,0,.453,1.236,4.565,4.565,0,0,0,1.689.968,27.034,27.034,0,0,0,3.707,1.009,24.976,24.976,0,0,1,6.281,2.08A7.864,7.864,0,0,1,811,303.726a7.605,7.605,0,0,1,.947,3.892,7.9,7.9,0,0,1-3.336,6.734q-3.336,2.451-9.391,2.451A24.921,24.921,0,0,1,792.711,315.938Z" transform="translate(-787.48 -286.736)" fill="#fff"/><path d="M867.438,312.913v5.642a10.393,10.393,0,0,1-3.872.618q-4.242,0-6.446-2.389a9.41,9.41,0,0,1-2.2-6.631V303.4H851v-5.478h3.913v-4.737l7.455-2.389v7.126H867.4V303.4h-5.025v6.755a2.8,2.8,0,0,0,.885,2.245,3.257,3.257,0,0,0,2.2.762A10.656,10.656,0,0,0,867.438,312.913Z" transform="translate(-825.22 -289.147)" fill="#fff"/><path d="M901.454,327.441a9.344,9.344,0,0,1-4-3.789,12.742,12.742,0,0,1,0-11.615,9.35,9.35,0,0,1,4-3.789,13.3,13.3,0,0,1,6.1-1.318,12.908,12.908,0,0,1,6.034,1.339,9.542,9.542,0,0,1,3.975,3.789,12.654,12.654,0,0,1,0,11.574,9.535,9.535,0,0,1-3.975,3.789,12.9,12.9,0,0,1-6.034,1.339A13.289,13.289,0,0,1,901.454,327.441Zm10.132-9.6a6.524,6.524,0,0,0-1.009-4.016,3.6,3.6,0,0,0-3.027-1.3,3.7,3.7,0,0,0-3.089,1.3,6.421,6.421,0,0,0-1.029,4.016,6.5,6.5,0,0,0,1.029,4.057,3.7,3.7,0,0,0,3.089,1.3Q911.586,323.2,911.586,317.845Z" transform="translate(-851.987 -298.733)" fill="#fff"/><path d="M976.189,307.586v6.8a8.1,8.1,0,0,0-3.418-.824q-4.7,0-4.7,4.489v10.5h-7.455v-14.91a48.955,48.955,0,0,0-.33-6.013h7l.453,3.5a5.85,5.85,0,0,1,2.389-2.966,6.85,6.85,0,0,1,3.707-1.029A4.947,4.947,0,0,1,976.189,307.586Z" transform="translate(-890.149 -298.854)" fill="#fff"/><path d="M1036.278,309.154q1.75,2.225,1.75,6.8v12.48h-7.455V316.156a4.924,4.924,0,0,0-.556-2.677,2.094,2.094,0,0,0-1.874-.824,2.863,2.863,0,0,0-2.43,1.153,5.336,5.336,0,0,0-.865,3.254V328.43h-7.455V316.156a4.786,4.786,0,0,0-.576-2.677,2.106,2.106,0,0,0-1.854-.824,2.807,2.807,0,0,0-2.409,1.153,5.431,5.431,0,0,0-.844,3.254V328.43h-7.455V313.52a48.963,48.963,0,0,0-.329-6.013h7l.371,2.924a6.452,6.452,0,0,1,2.656-2.595,8.277,8.277,0,0,1,3.934-.906q4.488,0,6.1,3.789a8.236,8.236,0,0,1,2.945-2.76,7.979,7.979,0,0,1,3.975-1.03A6.42,6.42,0,0,1,1036.278,309.154Z" transform="translate(-916.072 -298.733)" fill="#fff"/><path d="M1115.823,308.351l-9.6,21.665a14.331,14.331,0,0,1-4.757,6.2,15.544,15.544,0,0,1-7.064,2.45l-1.73-5.684A13.285,13.285,0,0,0,1097,331.5a5.731,5.731,0,0,0,2.183-2.512l.535-1.153-8.526-19.481h7.744l4.7,12.068,4.86-12.068Z" transform="translate(-967.919 -299.578)" fill="#fff"/></g><g transform="translate(679.48 287.302)"><path d="M708.676,291.767V295.4H683.117a3.636,3.636,0,0,1-3.637-3.637V288.13h25.559A3.639,3.639,0,0,1,708.676,291.767Z" transform="translate(-679.48 -288.13)" fill="#fff"/><path d="M708.676,345.617v3.637H683.117a3.636,3.636,0,0,1-3.637-3.637V341.98h25.559A3.636,3.636,0,0,1,708.676,345.617Z" transform="translate(-679.48 -320.123)" fill="#fff"/><path d="M708.676,318.7v3.637H683.117a3.636,3.636,0,0,1-3.637-3.637V315.06h25.559A3.636,3.636,0,0,1,708.676,318.7Z" transform="translate(-679.48 -304.129)" fill="#fff"/></g></g></svg>
+                                </a>
+                            </div>
+                            
+                            <div class="link-footer">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#Vantagens" title="Vantagens">Vantagens</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#Caracter" title="Características">Características</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#Blog" title="Blog">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#Fale" title="Fale Conosco">Fale Conosco</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            
+                            <div class="social">
+                                <ul class="navbar-nav mr-auto">
+                                    <li class="nav-item active">
+                                        <a class="nav-link" href="#" title="Facebook">Facebook</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link last" href="#" title="Instagram">Instagram</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
                     </div>
                     <div class="copyright">
                         <div>Copyright copyright 2019. Todos os direitos reservados</div>
@@ -22,7 +46,13 @@
             </div>
         </section>
 
-    </div><!-- #header -->
+    </div><!-- #container-fluid -->
+
+    <!-- JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/resources/js/bootstrap.min.js"></script>
 
 <?php wp_footer(); ?>
 
--- index.php
+++ index.php
@@ -1,5 +1,164 @@
 <?php
 
-get_header();
+get_header(); ?>
 
-get_footer();
+<div class="container-fluid">
+        <section class="layer-1 go-layer">
+            <div class="wrapper-1 go-wrapper">
+                <div class="content-1 go-content container-md container-sm">
+                    <div class="right-side bottom-side">
+                        <div class="block">
+                            <h1><span class="color-sec">Crie relatórios para</span> Oracle E-Business Suite</h1>
+                            <button type="button" class="btn btn-primary">Veja como funciona</button>
+                        </div>
+                    </div>
+                    <div class="left-side top-side">
+                        <img src="<?php echo get_template_directory_uri(); ?>/resources/images/pc.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-2 go-layer">
+            <div class="wrapper-2 go-wrapper">
+                <div class="content-2 go-content container-md container-sm">
+                    <div class="right-side top-side">
+                        <h3>Você já passou por algum desses problemas?</h3>
+                        <ul>
+                            <li>Atraso pela dificuldade de por um relatório em produção</li>
+                            <li>Pessoas de negócios executando consultas em ferramentas de SQL</li>
+                            <li>Sua equipe demora muito para produzir relatórios</li>
+                            <li>Dificuldade de introduzir um novo membro na equipe para desenvolver relatórios</li>
+                            <li>Sentiu que as ferramentas de relatórios de EBS são velhas e ultrapassadas</li>
+                        </ul>
+                    </div>
+                    <div class="left-side bottom-side">
+                        <h2><img src="<?php echo get_template_directory_uri(); ?>/resources/images/logo-stormy.svg" alt=""></h2>
+                        <p>O <b>Stormy</b> é uma ferramenta de relatórios projetada para facilitar a construção de relatórios no <b>Oracle E-Business Suite</b></p>
+                        <p>Construída com tecnologias e técnicas focadas em <b>otimizar o trabalho</b> de desenvolvedores e dos usuários finais do negócio.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-3 go-layer" id="Vantagens">
+            <div class="wrapper-3 go-wrapper">
+                <div class="content-3 go-content container-md container-sm">
+                    <h4>Vantagens</h4>
+                    <div class="box-group">
+                        <div class="a">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/vantagem-a.png" alt=""></div>
+                            <div class="title-group">PRODUTIVIDADE</div>
+                            <p>Acelere suas entregas. Permita que seu time construa relatórios em menor espaço de tempo.</p>
+                        </div>
+                        <div class="b">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/vantagem-b.png" alt=""></div>
+                            <div class="title-group">FÁCIL APRENDIZADO</div>
+                            <p>A baixa curva de aprendizagem permite que novos membros da equipe consiga produzir relatórios.</p>
+                        </div>
+                        <div class="c">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/vantagem-c.png" alt=""></div>
+                            <div class="title-group">AGILIDADE</div>
+                            <p>Crie e disponibilize relatórios em poucos cliques.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-4 go-layer" id="#Caracter">
+            <div class="wrapper-4 go-wrapper">
+                <div class="content-4 go-content container-md container-sm">
+                    <h4>CARACTERÍSTICAS</h4>
+                    <div class="box-group">
+                        <div class="right-side top-side">
+                            <div class="left-bar carac-box">
+                                <div class="title-group">Pré-visualização de Dados</div>
+                                <p>Permite ao usuário pré-visualizar as informações sem ter que exportar o relatório PDF ou Excel.</p>
+                            </div>
+                            <div class="left-bar carac-box">
+                                <div class="title-group">Formatos</div>
+                                <p>A baixa curva de aprendizado permite que novos membros da equipe consiga produzir relatórios.</p>
+                            </div>
+                        </div>
+                        <div class="left-side bottom-side">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/layout-a.png" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-5 go-layer" id="Blog">
+            <div class="wrapper-5 go-wrapper">
+                <div class="content-5 go-content container-md container-sm">
+                    <div class="box-group">
+                        <div class="right-side top-side">
+                            <div class="left-bar carac-box">
+                                <div class="title-group">Personalização</div>
+                                <p>Permite o usuário customizar as ordem, exibição e o agrupamento.</p>
+                            </div>
+                            <div class="left-bar carac-box">
+                                <div class="title-group">Deployment</div>
+                                <p>Basta escolher a responsabilidade e o menu, que o Stormy irá disponibilizar o relatório para o usuário final imediatamente.</p>
+                            </div>
+                        </div>
+                        <div class="left-side bottom-side">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/layout-b.png" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-6 go-layer">
+            <div class="wrapper-6 go-wrapper">
+                <div class="content-6 go-content container-md container-sm">
+                    <h4>DEPOIMENTOS</h4>
+                    <div class="box-group">
+                        <div class="left-side top-side">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/pic.png" alt=""></div>
+                        </div>
+                        <div class="right-side bottom-side">
+                            <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/logo-unimed.png" alt=""></div>
+                            <p>Após a implantação do Stormy, eliminamos toda a burocracia e esforço para criar, empacotar e publicar relatórios, aumentando significativamente o volume de entregas, a satisfação do usuário e reduzindo os erros advindos da tecnologia utilizada anteriormente (XML Publisher e Query Report)</p>
+                            <p class="name dep">Plínio Girão <span class="title dep">GERENTE DE TI DA UNIMED FORTALEZA</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-7 go-layer">
+            <div class="wrapper-7 go-wrapper">
+                <div class="content-7 go-content container-md container-sm">
+                    <h2>Parceria que agrega!</h2>
+                    <div><img src="<?php echo get_template_directory_uri(); ?>/resources/images/logo-ivia.png" alt=""></div>
+                </div>
+            </div>
+        </section>
+        <section class="layer-8 go-layer" id="Fale">
+            <div class="wrapper-8 go-wrapper">
+                <div class="content-8 go-content container-md container-sm">
+                    <div class="left-side top-side">
+                        <h2>Fale Conosco</h2>
+                        <p>Vivamus eget aliquam dui. Integer eu arcu vel arcu suscipit ultrices quis non mauris.</p>
+                    </div>
+                    <div class="right-side bottom-side">
+                        <form>
+                            <div class="form-group">
+                              <input type="text" class="form-control" id="nome" placeholder="Nome Completo">
+                            </div>
+                            <div class="form-group two">
+                                <input type="email" class="form-control" id="email" placeholder="E-mail">
+                                <input type="text" class="form-control" id="email" placeholder="Empresa">
+                            </div>
+                            <div class="form-group two">
+                                <input type="text" class="form-control" id="email" placeholder="Cargo">
+                                <input type="text" class="form-control" id="email" placeholder="Telefone (com DDD)">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"  placeholder="Mensagem"></textarea>
+                              </div>
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                          </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<?php get_footer();
